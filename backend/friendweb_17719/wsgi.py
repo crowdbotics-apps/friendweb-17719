84a45f33bfd42fb339cfc7bd3530e756138eb8b0
Crowdbotics <team@crowdbotics.com>
2020-06-02 23:22:46 +0000

    Update models

--- backend/friendweb_17719/wsgi.py
+++ backend/friendweb_17719/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friendweb_17719.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friendweb_17719.settings")
 
 application = get_wsgi_application()
